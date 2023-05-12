8da29af574a4cc0e24ee0ab338f2e6b4ce457272
Seryeong <srlimvp@gmail.com>
2023-05-12 23:47:33 +0900

    5/12

---
+++ class2/2805 나무 자르기.py
@@ -0,0 +1,20 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+h = list(map(int, sys.stdin.readline().split()))
+
+start = 0
+end = max(h)
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+
+    for i in h:
+        if i >= mid:
+            total += i - mid
+
+    if total >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
