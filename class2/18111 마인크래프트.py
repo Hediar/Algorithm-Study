cc3c0fe91b17c466a1d697b9e44b288f48389099
Seryeong Lee <srlimvp@gmail.com>
2023-06-01 23:37:09 +0900

    faster code

--- class2/18111 마인크래프트.py
+++ class2/18111 마인크래프트.py
@@ -32,3 +32,37 @@ for v in range(min_h, max_h + 1):
             res[1] = v  # 최소시간일 때의 현재 높이
 
 print(*res)  # 각 요소를 공백으로 구분하여 출력
+
+""" 시간 빠른 코드 
+import sys
+
+input = sys.stdin.readline
+
+N, M, B = map(int, input().split())
+MAT = []
+HEIS = [0 for _ in range(257)]
+for n in range(N):
+    T = list(map(int, input().split()))
+    for m in range(M):
+        HEIS[T[m]] += 1
+    MAT.append(T)
+
+m_res, h_res = float('inf'), 0
+for h in range(257):
+    p = 0
+    m = 0
+    for b in range(257):
+        if h > b:
+            p += (h - b) * HEIS[b]
+        else:
+            m += (b - h) * HEIS[b]
+    inven = B + m - p
+    if inven < 0:
+        continue
+    t = m * 2 + p
+    if t <= m_res:
+        m_res = t
+        h_res = h
+
+print(m_res, h_res)
+"""
