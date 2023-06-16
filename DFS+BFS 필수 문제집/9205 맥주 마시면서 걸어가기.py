3666921fd207c0d15550e34a1f06158535bde29f
Seryeong Lee <srlimvp@gmail.com>
2023-06-17 03:03:06 +0900

    6/15

---
+++ DFS+BFS 필수 문제집/9205 맥주 마시면서 걸어가기.py
@@ -0,0 +1,51 @@
+"""
+- 입력
+테스트 케이스 수 t
+
+편의점 개수 n
+상근이네 집 좌표
+편의점 좌표
+페스티벌 좌표 
+
+- 출력
+페스티벌 도착 시 happy
+맥주가 바닥나서 이동을 못할 경우 sad
+"""
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+T = int(input())
+
+
+def bfs():
+    q = deque()
+    q.append((home_x, home_y))
+
+    while q:
+        x, y = q.popleft()
+        if abs(x-festival_x) + abs(y-festival_y) <= 1000:  # 거리가 맥주를 다 마실때 내에 들어갈때
+            print('happy')
+            return
+        for i in range(n):
+            if not visited[i]:
+                n_x, n_y = g[i]
+                if abs(n_x - x) + abs(y-n_y) <= 1000:
+                    visited[i] = 1
+                    q.append((n_x, n_y))
+    print("sad")
+    return
+
+
+for _ in range(T):
+    n = int(input())  # 편의점 개수
+    home_x, home_y = map(int, input().split())  # 상근이네 집
+    g = []
+    for _ in range(n):  # 편의점
+        x, y = map(int, input().split())
+        g.append((x, y))
+
+    festival_x, festival_y = map(int, input().split())  # 페스티벌
+    visited = [0 for _ in range(n+1)]
+
+    bfs()
