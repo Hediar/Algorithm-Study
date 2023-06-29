69a83926f53b2116cb3ce0a753531a0e65d47715
Seryeong Lee <srlimvp@gmail.com>
2023-06-30 03:50:08 +0900

    6/29

--- class3/9095 1,2,3 더하기.py
+++ class3/9095 1,2,3 더하기.py
@@ -2,3 +2,17 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    def dp(n):
+        d = [0]*12 # n < 11
+        d[1] = 1
+        d[2] = 2
+        d[3] = 4
+
+        for i in range(4, n+1):
+            d[i] = d[i-1] + d[i-2] + d[i-3]
+        return d[n]
+    
+    print(dp(n))
