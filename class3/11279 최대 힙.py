48208a11353b1ab8b185258e5804d44e38ed876b
Seryeong Lee <srlimvp@gmail.com>
2023-07-06 01:37:52 +0900

    7/5

---
+++ class3/11279 최대 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+n = int(sys.stdin.readline())
+
+max_heap = []
+
+for i in range(n):
+    num = int(input())
+    if num != 0: # 숫자일 때 추가연산
+        heapq.heappush(max_heap, (-num))
+        
+    else: 
+        if max_heap: # 값이 있을 때, 제거 
+            print(-1 * heapq.heappop(max_heap))
+        else:
+            print(0)
